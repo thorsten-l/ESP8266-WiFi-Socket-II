eb15199c498907af06ee10be5f8167603e336513
Thorsten Ludewig <th@l9g.de>
2018-12-29 17:14:01 +0100

    firmware release 2.0.5

--- firmware/OTA_UPLOAD.sh
+++ firmware/OTA_UPLOAD.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 $HOME/.platformio/penv/bin/python \
   $HOME/.platformio/packages/tool-espotapy/espota.py \
-  --debug --progress -i "wifi-socket-1.local" \
-  --auth="otapass" -f "firmware-1.1.6.bin"
+  --debug --progress -i "wifi-socket-ii.local" \
+  --auth="otapass" -f "firmware-2.0.5.bin"
