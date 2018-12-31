cb63c8508eb55572ef88f4ce7bf29cb0dd579eb7
Thorsten Ludewig <th@l9g.de>
2018-12-31 12:47:55 +0100

    release 2.0.6

--- firmware/OTA_UPLOAD.sh
+++ firmware/OTA_UPLOAD.sh
@@ -2,4 +2,4 @@
 $HOME/.platformio/penv/bin/python \
   $HOME/.platformio/packages/tool-espotapy/espota.py \
   --debug --progress -i "wifi-socket-ii.local" \
-  --auth="otapass" -f "firmware-2.0.5.bin"
+  --auth="otapass" -f "firmware-2.0.6.bin"
--- firmware/USB_UPLOAD.sh
+++ firmware/USB_UPLOAD.sh
@@ -1,2 +1,2 @@
  #!/bin/sh
- $HOME/.platformio/packages/tool-esptool/esptool -cd ck -cb 115200 -cp /dev/cu.SLAB_USBtoUART -cf firmware-2.0.5.bin
+ $HOME/.platformio/packages/tool-esptool/esptool -cd ck -cb 115200 -cp /dev/cu.SLAB_USBtoUART -cf firmware-2.0.6.bin
