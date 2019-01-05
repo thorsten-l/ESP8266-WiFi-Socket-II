efb63452372af3ae449b3dd71184b1578d886338
Thorsten Ludewig <th@l9g.de>
2019-01-05 19:34:32 +0100

    release 2.1.0

--- lib/App/App.hpp
+++ lib/App/App.hpp
@@ -7,7 +7,7 @@
 #define LOG1( format, x) Serial.printf( "(%ld) " format, millis(), x )
 
 #define APP_NAME "WiFi Socket II"
-#define APP_VERSION "2.1.0dev1"
+#define APP_VERSION "2.1.0"
 #define APP_AUTHOR "Dr. Thorsten Ludewig <t.ludewig@gmail.com>"
 #define APP_CONFIG_FILE "/config.bin"
 
