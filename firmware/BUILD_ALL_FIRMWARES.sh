0838a3f9f23489482156ebab4590360e9b9b8ed6
Thorsten Ludewig <th@l9g.de>
2019-05-19 23:33:45 +0200

    build pio_env bw_shp6 also

--- firmware/BUILD_ALL_FIRMWARES.sh
+++ firmware/BUILD_ALL_FIRMWARES.sh
@@ -6,7 +6,7 @@ fw_version=`awk '/APP_VERSION/ { print $3 }' lib/App/App.hpp | sed -e 's/"//g'`
 
 echo "Firmware Version: $fw_version"
 
-for pioenv in dev1 dev1lite obi_v1 obi_v2
+for pioenv in dev1 dev1lite obi_v1 obi_v2 bw_shp6
 do
   echo "Build environment: $pioenv"
   platformio run -s -e $pioenv
