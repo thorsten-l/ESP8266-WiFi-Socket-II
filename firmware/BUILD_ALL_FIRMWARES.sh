c4186e663aa9c594729a7c358311fcb2d8d3b7e0
Thorsten Ludewig <th@l9g.de>
2019-06-10 12:55:06 +0200

    firmware version is now automatically created

--- firmware/BUILD_ALL_FIRMWARES.sh
+++ firmware/BUILD_ALL_FIRMWARES.sh
@@ -2,13 +2,9 @@
 
 cd ..
 
-fw_version=`awk '/APP_VERSION/ { print $3 }' lib/App/App.hpp | sed -e 's/"//g'`
-
-echo "Firmware Version: $fw_version"
-
 for pioenv in dev1 dev1lite obi_v1 obi_v2 bw_shp6 shelly1
 do
   echo "Build environment: $pioenv"
   platformio run -s -e $pioenv
-  cp ".pioenvs/$pioenv/firmware.bin" "firmware/firmware-$fw_version.$pioenv.bin"
+  cp .pioenvs/$pioenv/firmware-*.bin firmware
 done
