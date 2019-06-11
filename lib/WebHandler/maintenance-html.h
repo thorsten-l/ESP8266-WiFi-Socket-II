de307deaedba5bfa800597f89a2b0ba1981b885c
Thorsten Ludewig <th@l9g.de>
2019-06-11 19:52:18 +0200

    ".bin" added to firmware file name

--- lib/WebHandler/maintenance-html.h
+++ lib/WebHandler/maintenance-html.h
@@ -57,7 +57,7 @@ const char MAINTENANCE_HTML[] PROGMEM =
                         "<legend>Upload new firmware</legend>\n"
                         "<div>\n"
                             "<input class='inputfile' id='pgid34' type='file' name='update'>\n"
-                            "<label for='pgid34'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='20' viewBox='0 0 24 20'><path d='M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z'/></svg> <span>Choose a <b><tt>%pioenv_name%</tt></b> firmware file&hellip;</span></label>\n"
+                            "<label for='pgid34'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='20' viewBox='0 0 24 20'><path d='M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z'/></svg> <span>Choose a <b><tt>%pioenv_name%.bin</tt></b> firmware file&hellip;</span></label>\n"
                         "</div>\n"
                         "<p><input class='pure-button pure-button-primary' type='submit' value='Upload'></p>\n"
                     "</fieldset>\n"
