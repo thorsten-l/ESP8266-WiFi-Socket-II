ad76eadb174192d9c1d3ece9fed503fee3a7cb0c
Thorsten Ludewig <th@l9g.de>
2019-06-12 18:45:50 +0200

    clean pioenv bevor new build

--- firmware/BUILD_ALL_FIRMWARES.sh
+++ firmware/BUILD_ALL_FIRMWARES.sh
@@ -5,6 +5,7 @@ cd ..
 for pioenv in dev1 dev1lite obi_v1 obi_v2 bw_shp6 shelly1
 do
   echo "Build environment: $pioenv"
+  platformio run -s -t clean -e $pioenv
   platformio run -s -e $pioenv
   cp .pioenvs/$pioenv/firmware-*.bin firmware
 done
